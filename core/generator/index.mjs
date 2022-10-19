88a7e0c406f8f86bd7d3fbf4fef2a84f3f1e1a5a
Dave Honneffer <pearofducks@gmail.com>
2022-10-19 16:19:46 +0200

    Cleanup

--- core/generator/index.mjs
+++ core/generator/index.mjs
@@ -6,22 +6,20 @@ import { LAYER_DEFAULT, LAYER_PREFLIGHTS } from "../constants.mjs";
 const { version } = pkg
 
 export class UnoGenerator {
+  version = version;
+  _cache = new Map();
+  blocked = new Set();
+  parentOrders = new Map();
+  events = createNanoEvents();
+
   constructor(userConfig = {}, defaults = {}) {
-    this.userConfig = userConfig;
-    this.defaults = defaults;
-    this.version = version;
-    this._cache = /* @__PURE__ */ new Map();
-    this.blocked = /* @__PURE__ */ new Set();
-    this.parentOrders = /* @__PURE__ */ new Map();
-    this.events = createNanoEvents();
     this.config = resolveConfig(userConfig, defaults);
-    this.events.emit("config", this.config);
+    this.events.emit('config', this.config);
   }
+
   setConfig(userConfig, defaults) {
-    if (!userConfig)
-      return;
-    if (defaults)
-      this.defaults = defaults;
+    if (!userConfig) return;
+    if (defaults) this.defaults = defaults;
     this.userConfig = userConfig;
     this.blocked.clear();
     this.parentOrders.clear();
@@ -29,23 +27,24 @@ export class UnoGenerator {
     this.config = resolveConfig(userConfig, this.defaults);
     this.events.emit("config", this.config);
   }
+
   async applyExtractors(code, id, set = /* @__PURE__ */ new Set()) {
     const context = {
-      get original() {
-        return code;
-      },
+      get original() { return code; },
       code,
       id
     };
+
     for (const extractor of this.config.extractors) {
       const result = await extractor.extract(context);
       if (result) {
-        for (const token of result)
-          set.add(token);
+        for (const token of result) set.add(token);
       }
     }
+
     return set;
   }
+
   makeContext(raw, applied) {
     const context = {
       rawSelector: raw,
@@ -58,34 +57,51 @@ export class UnoGenerator {
     };
     return context;
   }
+
   async parseToken(raw, alias) {
     if (this.blocked.has(raw)) return;
     const cacheKey = `${raw}${alias ? ` ${alias}` : ""}`;
+
+    // use cache if possible
     if (this._cache.has(cacheKey)) return this._cache.get(cacheKey);
+
     let current = raw;
     for (const p of this.config.preprocess) current = p(raw);
+
     if (this.isBlocked(current)) {
       this.blocked.add(raw);
       this._cache.set(cacheKey, null);
       return;
     }
+
     const applied = this.matchVariants(raw, current);
+
     if (!applied || this.isBlocked(applied[1])) {
       this.blocked.add(raw);
       this._cache.set(cacheKey, null);
       return;
     }
+
     const context = this.makeContext(raw, [alias || applied[0], applied[1], applied[2], applied[3]]);
-    if (this.config.details)
-      context.variants = [...applied[3]];
+
+    if (this.config.details) context.variants = [...applied[3]];
+
+    // expand shortcuts
     const expanded = this.expandShortcut(context.currentSelector, context);
-    const utils = expanded ? await this.stringifyShortcuts(context.variantMatch, context, expanded[0], expanded[1]) : (await this.parseUtil(context.variantMatch, context))?.map((i) => this.stringifyUtil(i, context)).filter(notNull);
+    const utils = expanded
+      ? await this.stringifyShortcuts(context.variantMatch, context, expanded[0], expanded[1])
+      // no shortcuts
+      : (await this.parseUtil(context.variantMatch, context))?.map((i) => this.stringifyUtil(i, context)).filter(notNull);
+
     if (utils?.length) {
       this._cache.set(cacheKey, utils);
       return utils;
     }
+
+    // null cache for unmatched result
     this._cache.set(cacheKey, null);
   }
+
   async generate(input, options = {}) {
     const {
       id,
@@ -95,48 +111,61 @@ export class UnoGenerator {
       blocklist = false,
       minify = false
     } = options;
-    const tokens = isString(input) ? await this.applyExtractors(input, id) : Array.isArray(input) ? new Set(input) : input;
+
+    const tokens = isString(input)
+      ? await this.applyExtractors(input, id)
+      : Array.isArray(input)
+        ? new Set(input)
+        : input;
+
     if (safelist) this.config.safelist.forEach((s) => tokens.add(s));
     if (blocklist) this.config.blocklist.forEach((s) => tokens.delete(s));
+
     const nl = minify ? "" : "\n";
-    const layerSet = /* @__PURE__ */ new Set([LAYER_DEFAULT]);
-    const matched = /* @__PURE__ */ new Set();
-    const sheet = /* @__PURE__ */ new Map();
+
+    const layerSet = new Set([LAYER_DEFAULT]);
+    const matched = new Set();
+    const sheet = new Map();
     let preflightsMap = {};
+
     const tokenPromises = Array.from(tokens).map(async (raw) => {
-      if (matched.has(raw))
-        return;
+      if (matched.has(raw)) return;
+
       const payload = await this.parseToken(raw);
-      if (payload == null)
-        return;
+      if (payload == null) return;
+
       matched.add(raw);
+
       for (const item of payload) {
         const parent = item[3] || "";
         const layer = item[4]?.layer;
-        if (!sheet.has(parent))
-          sheet.set(parent, []);
+        if (!sheet.has(parent)) sheet.set(parent, []);
         sheet.get(parent).push(item);
-        if (layer)
-          layerSet.add(layer);
+        if (layer) layerSet.add(layer);
       }
     });
+
     const preflightPromise = (async () => {
-      if (!preflights)
-        return;
+      if (!preflights) return;
+
       const preflightContext = {
         generator: this,
         theme: this.config.theme
       };
-      const preflightLayerSet = /* @__PURE__ */ new Set([]);
+
+      const preflightLayerSet = new Set([]);
       this.config.preflights.forEach(({ layer = LAYER_PREFLIGHTS }) => {
         layerSet.add(layer);
         preflightLayerSet.add(layer);
       });
+
       preflightsMap = Object.fromEntries(
         await Promise.all(Array.from(preflightLayerSet).map(
           async (layer) => {
             const preflights2 = await Promise.all(
-              this.config.preflights.filter((i) => (i.layer || LAYER_PREFLIGHTS) === layer).map(async (i) => await i.getCSS(preflightContext))
+              this.config.preflights
+                .filter((i) => (i.layer || LAYER_PREFLIGHTS) === layer)
+                .map(async (i) => await i.getCSS(preflightContext))
             );
             const css = preflights2.filter(Boolean).join(nl);
             return [layer, css];
@@ -144,47 +173,58 @@ export class UnoGenerator {
         ))
       );
     })();
+
     await Promise.all([
       ...tokenPromises,
       preflightPromise
     ]);
+
     const layers = this.config.sortLayers(
-      Array.from(layerSet).sort((a, b) => (this.config.layers[a] ?? 0) - (this.config.layers[b] ?? 0) || a.localeCompare(b))
+      Array.from(layerSet)
+        .sort((a, b) => (this.config.layers[a] ?? 0) - (this.config.layers[b] ?? 0) || a.localeCompare(b))
     );
+
     const layerCache = {};
     const getLayer = (layer) => {
-      if (layerCache[layer])
-        return layerCache[layer];
-      let css = Array.from(sheet).sort((a, b) => (this.parentOrders.get(a[0]) ?? 0) - (this.parentOrders.get(b[0]) ?? 0) || a[0]?.localeCompare(b[0] || "") || 0).map(([parent, items]) => {
-        const size = items.length;
-        const sorted = items.filter((i) => (i[4]?.layer || LAYER_DEFAULT) === layer).sort((a, b) => a[0] - b[0] || (a[4]?.sort || 0) - (b[4]?.sort || 0) || a[1]?.localeCompare(b[1] || "") || a[2]?.localeCompare(b[2] || "") || 0).map(([, selector, body, , meta, , variantNoMerge]) => {
-          const scopedSelector = selector ? applyScope(selector, scope) : selector;
-          return [
-            [[scopedSelector ?? "", meta?.sort ?? 0]],
-            body,
-            !!(variantNoMerge ?? meta?.noMerge)
-          ];
-        });
-        if (!sorted.length)
-          return undefined;
-        const rules = sorted.reverse().map(([selectorSortPair, body, noMerge], idx) => {
-          if (!noMerge && this.config.mergeSelectors) {
-            for (let i = idx + 1; i < size; i++) {
-              const current = sorted[i];
-              if (current && !current[2] && (selectorSortPair && current[0] || selectorSortPair == null && current[0] == null) && current[1] === body) {
-                if (selectorSortPair && current[0])
-                  current[0].push(...selectorSortPair);
-                return null;
+      if (layerCache[layer]) return layerCache[layer];
+
+      let css = Array.from(sheet)
+        .sort((a, b) => (this.parentOrders.get(a[0]) ?? 0) - (this.parentOrders.get(b[0]) ?? 0) || a[0]?.localeCompare(b[0] || "") || 0)
+        .map(([parent, items]) => {
+          const size = items.length;
+          const sorted = items
+            .filter((i) => (i[4]?.layer || LAYER_DEFAULT) === layer)
+            .sort((a, b) => a[0] - b[0] || (a[4]?.sort || 0) - (b[4]?.sort || 0) || a[1]?.localeCompare(b[1] || "") || a[2]?.localeCompare(b[2] || "") || 0)
+            .map(([, selector, body,, meta,, variantNoMerge]) => {
+              const scopedSelector = selector ? applyScope(selector, scope) : selector;
+              return [
+                [[scopedSelector ?? "", meta?.sort ?? 0]],
+                body,
+                !!(variantNoMerge ?? meta?.noMerge)
+              ];
+            });
+          if (!sorted.length) return undefined;
+          const rules = sorted
+            .reverse()
+            .map(([selectorSortPair, body, noMerge], idx) => {
+              if (!noMerge && this.config.mergeSelectors) {
+                for (let i = idx + 1; i < size; i++) {
+                  const current = sorted[i];
+                  if (current && !current[2] && (selectorSortPair && current[0] || selectorSortPair == null && current[0] == null) && current[1] === body) {
+                    if (selectorSortPair && current[0]) current[0].push(...selectorSortPair);
+                    return null;
+                  }
+                }
               }
-            }
-          }
-          const selectors = selectorSortPair ? uniq(
-            selectorSortPair.sort((a, b) => a[1] - b[1] || a[0]?.localeCompare(b[0] || "") || 0).map((pair) => pair[0]).filter(Boolean)
-          ) : [];
-          return selectors.length ? `${selectors.join(`,${nl}`)}{${body}}` : body;
-        }).filter(Boolean).reverse().join(nl);
-        if (!parent)
-          return rules;
+            const selectors = selectorSortPair
+              ? uniq(selectorSortPair
+                .sort((a, b) => a[1] - b[1] || a[0]?.localeCompare(b[0] || "") || 0)
+                .map((pair) => pair[0])
+                .filter(Boolean))
+              : [];
+            return selectors.length ? `${selectors.join(`,${nl}`)}{${body}}` : body;
+          }).filter(Boolean).reverse().join(nl);
+        if (!parent) return rules;
         const parents = parent.split(" $$ ");
         return `${parents.join("{")}{${nl}${rules}${nl}}${parents.map((_) => "").join("}")}`;
       }).filter(Boolean).join(nl);
@@ -198,17 +238,16 @@ export class UnoGenerator {
       return includes.filter((i) => !excludes?.includes(i)).map((i) => getLayer(i) || "").filter(Boolean).join(nl);
     };
     return {
-      get css() {
-        return getLayers();
-      },
+      get css() { return getLayers(); },
       layers,
       matched,
       getLayers,
       getLayer
     };
   }
+
   matchVariants(raw, current) {
-    const variants = /* @__PURE__ */ new Set();
+    const variants = new Set();
     const handlers = [];
     let processed = current || raw;
     let applied = false;
@@ -235,6 +274,7 @@ export class UnoGenerator {
     }
     return [raw, processed, handlers, variants];
   }
+
   applyVariants(parsed, variantHandlers = parsed[4], raw = parsed[1]) {
     const handler = [...variantHandlers].sort((a, b) => (a.order || 0) - (b.order || 0)).reverse().reduce(
       (previous, v) => (input) => {
@@ -275,6 +315,7 @@ export class UnoGenerator {
     for (const p of this.config.postprocess) p(obj);
     return obj;
   }
+
   constructCustomCSS(context, body, overrideSelector) {
     const normalizedBody = normalizeCSSEntries(body);
     if (isString(normalizedBody)) return normalizedBody;
@@ -283,8 +324,10 @@ export class UnoGenerator {
     if (parent) return `${parent}{${cssBody}}`;
     return cssBody;
   }
+
   async parseUtil(input, context, internal = false) {
     const [raw, processed, variantHandlers] = isString(input) ? this.matchVariants(input) : input;
+    console.log({ raw, processed, variantHandlers })
     if (this.config.details) context.rules = context.rules ?? [];
     const staticMatch = this.config.rulesStaticMap[processed];
     if (staticMatch) {
@@ -307,7 +350,7 @@ export class UnoGenerator {
         unprefixed = processed.slice(meta.prefix.length);
       }
       const match = unprefixed.match(matcher);
-      console.log("MATCH", unprefixed, matcher, match)
+      // console.log("MATCH", unprefixed, matcher, match)
       if (!match) continue;
       const result = await handler(match, context);
       if (!result) continue;
@@ -321,15 +364,13 @@ export class UnoGenerator {
       }
     }
   }
+
   stringifyUtil(parsed, context) {
-    if (!parsed)
-      return;
-    if (isRawUtil(parsed))
-      return [parsed[0], undefined, parsed[1], undefined, parsed[2], this.config.details ? context : undefined, undefined];
+    if (!parsed) return;
+    if (isRawUtil(parsed)) return [parsed[0], undefined, parsed[1], undefined, parsed[2], this.config.details ? context : undefined, undefined];
     const { selector, entries, parent, layer: variantLayer, sort: variantSort, noMerge } = this.applyVariants(parsed);
     const body = entriesToCss(entries);
-    if (!body)
-      return;
+    if (!body) return;
     const { layer: metaLayer, sort: metaSort, ...meta } = parsed[3] ?? {};
     const ruleMeta = {
       ...meta,
@@ -339,8 +380,7 @@ export class UnoGenerator {
     return [parsed[0], selector, body, parent, ruleMeta, this.config.details ? context : undefined, noMerge];
   }
   expandShortcut(input, context, depth = 5) {
-    if (depth === 0)
-      return;
+    if (depth === 0) return;
     const recordShortcut = this.config.details ? (s) => {
       context.shortcuts = context.shortcuts ?? [];
       context.shortcuts.push(s);
@@ -358,8 +398,7 @@ export class UnoGenerator {
         }
       } else {
         const match = unprefixed.match(s[0]);
-        if (match)
-          result = s[1](match, context);
+        if (match) result = s[1](match, context);
         if (result) {
           meta = meta || s[2];
           recordShortcut(s);
