3f6c55a60c7d3fd46bda438ff8ad4156ff0ff647
Dave Honneffer <pearofducks@gmail.com>
2022-11-08 09:54:05 +0100

    Sure

--- play.js
+++ play.js
@@ -1,12 +1,22 @@
 import { createGenerator, escapeSelector } from './core/index.js'
 import presetMini from './preset-mini/index.mjs'
+import * as components from '@fabric-ds/css/component-classes'
 
 const uno = createGenerator({
   presets: [
-    presetMini({ jit: true })
+    presetMini({
+      // both of these need to be false
+      base: false,
+      jit: false
+    })
   ],
 })
 
-// const result = await uno.generate('', { preflights: false })
-const result = await uno.generate(['sm:bg-blue-600', 'sm:p-16'])
+const classes = Object.values(components).map(e => {
+  if (typeof e === 'object') return Object.values(e).map(e => e.split(/\s/))
+  return e.split(/\s/)
+}).flat(Infinity)
+
+const result = await uno.generate(classes, { preflights: false })
+// const result = await uno.generate(['sm:bg-blue-600', 'sm:p-16'])
 console.log(result.css)
